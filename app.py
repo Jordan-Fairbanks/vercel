dc29fbbf773bc6d06d8f7943e9779ba6a9f74b61
Jordan <fordanjairbanks@gmail.com>
2024-01-10 15:39:50 -0500

    set up bones of project.

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "home page route"
+
+@app.route('/calendar')
+def calendar():
+    return 'calendar route'
